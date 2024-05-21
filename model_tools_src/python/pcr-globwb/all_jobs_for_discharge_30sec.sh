ef7dc9367fb6d08dfc7c5b421fd0232728731fd5
Edwin Sutanudjaja <edwinkost@UU105378>
2024-05-21 19:51:19 +0200

    test with different number of cores

--- model_tools_src/python/pcr-globwb/all_jobs_for_discharge_30sec.sh
+++ model_tools_src/python/pcr-globwb/all_jobs_for_discharge_30sec.sh
@@ -6,16 +6,8 @@
 
 
 
-sbatch --export YEAR="1979" job_for_discharge_30sec.sh
-sbatch --export YEAR="1980" job_for_discharge_30sec.sh
-sbatch --export YEAR="1981" job_for_discharge_30sec.sh
-sbatch --export YEAR="1982" job_for_discharge_30sec.sh
-sbatch --export YEAR="1983" job_for_discharge_30sec.sh
-sbatch --export YEAR="1984" job_for_discharge_30sec.sh
-sbatch --export YEAR="1985" job_for_discharge_30sec.sh
-sbatch --export YEAR="1986" job_for_discharge_30sec.sh
-sbatch --export YEAR="1987" job_for_discharge_30sec.sh
-sbatch --export YEAR="1988" job_for_discharge_30sec.sh
-sbatch --export YEAR="1989" job_for_discharge_30sec.sh
-sbatch --export YEAR="1990" job_for_discharge_30sec.sh
+sbatch -n  32 --export YEAR="1979" job_for_discharge_30sec.sh
+sbatch -n  64 --export YEAR="1980" job_for_discharge_30sec.sh
+sbatch -n  96 --export YEAR="1981" job_for_discharge_30sec.sh
+sbatch -n 128 --export YEAR="1982" job_for_discharge_30sec.sh
 
