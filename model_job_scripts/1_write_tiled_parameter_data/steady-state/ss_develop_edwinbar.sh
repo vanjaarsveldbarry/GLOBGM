47c19aa174a0bdc0e01ee8e070ecf96a74305ca9
Edwin Sutanudjaja <sutan101@UU078667-ubuntu>
2024-05-28 13:33:44 +0200

    test job for arfan

---
+++ model_job_scripts/1_write_tiled_parameter_data/steady-state/ss_develop_edwinbar.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+#SBATCH -t 08:00:00
+
+#~ #SBATCH --output=ss_%j.out
+#~ #SBATCH --error=ss_%j.err
+
+#SBATCH -p genoa
+#SBATCH -N 1
+
+#SBATCH -n 192
+
+
+MODEL_SCRIPT_FOLDER="/home/edwinbar/github/edwinkost/GLOBGM/model_tools_src/python/pcr-globwb/"
+INI_FILE="/home/edwinbar/github/edwinkost/GLOBGM/model_input/1_write_tiled_parameter_data/develop/steady-state_config_develop_edwinbar.ini"
+
+TMP_FOLDER_FOR_THE_MODIFIED_INI_FILE="/scratch-shared/edwinbar/globgm_tile_map_files_for_arfan/map_input/steady-state/ini_files/"
+
+#~ cd {git_dir}/model_tools_src/python/pcr-globwb
+
+cd ${MODEL_SCRIPT_FOLDER}
+
+
+# activate the pcrglobwb conda environment
+. /home/edwin/load_all_default.sh
+
+
+python deterministic_runner_for_monthly_offline_globgm.py ${INI_FILE} debug steady-state-only tile_001-163 ${TMP_FOLDER_FOR_THE_MODIFIED_INI_FILE} &
+
