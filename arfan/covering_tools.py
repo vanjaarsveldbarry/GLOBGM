b39f5152d91d621dcb2d742f2f7c412275a1f8de
Edwin Sutanudjaja <edwinkost@UU105378>
2024-05-30 12:56:49 +0200

    make sure all elevation maps are covered

--- arfan/covering_tools.py
+++ arfan/covering_tools.py
@@ -13,12 +13,12 @@ for tile in tiles:
     
     print(tile)
     
-    pcr.setclone(top_uppermost_file)
-    
     top_uppermost_file    = tile + "/steady-state-only/maps/top_uppermost_layer.map"
     bottom_uppermost_file = tile + "/steady-state-only/maps/bottom_uppermost_layer.map"
     bottom_lowermost_file = tile + "/steady-state-only/maps/bottom_lowermost_layer.map"
     
+    pcr.setclone(top_uppermost_file)
+
     top_uppermost    = pcr.cover(pcr.readmap(top_uppermost_file   ), 0.0)
     bottom_uppermost = pcr.cover(pcr.readmap(bottom_uppermost_file), 0.0)
     bottom_lowermost = pcr.cover(pcr.readmap(bottom_lowermost_file), 0.0)
