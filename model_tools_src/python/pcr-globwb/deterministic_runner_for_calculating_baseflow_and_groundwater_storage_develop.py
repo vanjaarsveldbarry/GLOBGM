411dea47d8cb8f56dcec22cdb493bdb40a936572
Edwin Sutanudjaja <edwinkost@UU105378>
2024-06-05 15:23:44 +0200

    make scripts for calculating baseflow and groundwater storage

--- model_tools_src/python/pcr-globwb/deterministic_runner_for_calculating_baseflow_and_groundwater_storage_develop.py
+++ model_tools_src/python/pcr-globwb/deterministic_runner_for_calculating_baseflow_and_groundwater_storage_develop.py
@@ -21,28 +21,47 @@ os.makedirs(output_folder)
 tmp_dir = output_folder +  "/tmp/"
 os.makedirs(tmp_dir)
 
-# Note that the output from the MODFLOW6 has the convention that l1 (layer 1) is for the top one, while the pcraster version of GLOBGM use the l1 as the bottom one
+
+# the folder that contains modflow 6 output runs
 modflow6_output_folder = "/scratch-shared/marfan/globgm_ss/output/average/gswp3-w5e5/ss/mf6_post/"
 
 # clone map
 clone_map = "/projects/0/dfguu/users/edwin/data/pcrglobwb_input_arise/develop/global_30sec/routing/surface_water_bodies/version_2020-05-XX/lddsound_30sec_version_202005XX.map"
 
+# specific yield
+specificYield_input_file  = "/projects/0/dfguu/users/edwin/data/pcrglobwb_input_arise/develop/global_30sec/global_30sec/groundwater/properties/version_202312XX/specific_yield_aquifer_30sec_filled_v20231205.map"
+specificYield = pcr.readmap(specificYield_input_file)
+aquiferLayerPrimaryStorageCoefficient = 0.003
 
-# obtain the groundwater head for the bottom layer and their correponding storage
-groundwaterHead1 = None
-storGroundwater1 = None
+# obtain the groundwater head for the top layer (layer 2 of the pcraster version of GLOBGM)
+groundwaterHead2 = None
 for region in range(1, 4):
     
+    # Note that the output from the MODFLOW6 has the convention that l1 (layer 1) is for the top one, while the pcraster version of GLOBGM use the l1 as the bottom one
     ncFile = modflow6_output_folder + "/s0" + str(region) + "_hds_ss_l1.nc"
 
-    # ~ groundwaterHead1_inp = vos.netcdf2PCRobjCloneWithoutTime(ncFile  = ncFile, \
-                                                             # ~ varName = "automatic",\
-                                                             # ~ cloneMapFileName  = clone_map,\
-                                                             # ~ LatitudeLongitude = False,\
-                                                             # ~ specificFillValue = None,\
-                                                             # ~ absolutePath = None)
+    print(region)
+    
+    output = tmp_dir + 'temp.map'
+    warp = vos.gdalwarpPCR_with_mv(input = ncFile, output = output, cloneOut = clone_map, tmpDir = tmp_dir, isLddMap = False, isNominalMap = False, miss_val = "-9999")
+    groundwaterHead2_inp = pcr.readmap(output)
 
-    # ~ groundwaterHead1_inp = vos.readPCRmapClone(v = ncFile, cloneMapFileName = clone_map, tmpDir = tmp_dir, absolutePath = None, isLddMap = False, cover = None, isNomMap = False)
+
+    if region == 1:
+        groundwaterHead2 = groundwaterHead2_inp
+    else:
+        groundwaterHead2 = pcr.cover(groundwaterHead2, groundwaterHead2_inp)
+
+    # ~ pcr.aguila(groundwaterHead2)
+
+
+
+# obtain the groundwater head for the bottom layer (layer 1 of the pcraster version of GLOBGM)
+groundwaterHead1 = None
+for region in range(1, 4):
+    
+    # Note that the output from the MODFLOW6 has the convention that l1 (layer 1) is for the top one, while the pcraster version of GLOBGM use the l1 as the bottom one
+    ncFile = modflow6_output_folder + "/s0" + str(region) + "_hds_ss_l2.nc"
 
     print(region)
     
@@ -55,5 +74,22 @@ for region in range(1, 4):
         groundwaterHead1 = groundwaterHead1_inp
     else:
         groundwaterHead1 = pcr.cover(groundwaterHead1, groundwaterHead1_inp)
-    pcr.aguila(groundwaterHead1)
+
+    # ~ pcr.aguila(groundwaterHead1)
+    
+
+
+# calculating the corresponding groundwater storage for the top layer
+storage_coefficient_2 = specificYield
+storGroundwater2 = groundwaterHead2 * storage_coefficient_2
+pcr.aguila(storGroundwater2)
+
+# calculating the corresponding groundwater storage for the bottom layer
+confined_aquifer = pcr.cover(pcr.defined(storGroundwater1), pcr.boolean(0.0))
+storage_coefficient_1 = pcr.ifthenelse(confined_aquifer, aquiferLayerPrimaryStorageCoefficient, specificYield)
+storGroundwater1 = groundwaterHead1 * storage_coefficient_1
+pcr.aguila(storGroundwater1)
+
+totalStorGroundwater = pcr.cover(storGroundwater2, 0.0) + pcr.cover(storGroundwater1, 0.0)
+pcr.aguila(totalStorGroundwater)
 
