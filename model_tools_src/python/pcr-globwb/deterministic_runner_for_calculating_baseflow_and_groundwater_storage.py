4f71313d3cf1d430f85f67e3987a04a00006d65a
Edwin Sutanudjaja <edwinkost@UU105378>
2024-06-05 14:35:32 +0200

    make scripts for calculating baseflow and groundwater storage

--- model_tools_src/python/pcr-globwb/deterministic_runner_for_calculating_baseflow_and_groundwater_storage.py
+++ model_tools_src/python/pcr-globwb/deterministic_runner_for_calculating_baseflow_and_groundwater_storage.py
@@ -9,19 +9,60 @@ import datetime
 
 import pcraster as pcr
 
-from pcraster.framework import DynamicModel
-from pcraster.framework import DynamicFramework
+from globgm.virtualOS import vos
 
-from globgm.currTimeStep import ModelTime 
+# output folder for this analysis
+output_folder = "/scratch-shared/edwinaha/test_baseflow_and_storage/"
 
-import globgm.ncConverter_for_discharge_30sec as netcdf_writer 
-import globgm.virtualOS as vos 
+# make output and temporary folders
+if os.path.exists(output_folder): shutil.rmtree(output_folder)
+os.makedirs(output_folder)
+# - make temporary folder
+tmp_dir = output_folder +  "/tmp/"
+os.makedirs(tmp_dir)
 
-import logging
-logger = logging.getLogger(__name__)
+# Note that the output from the MODFLOW6 has the convention that l1 (layer 1) is for the top one, while the pcraster version of GLOBGM use the l1 as the bottom one
+modflow6_output_folder = "/scratch-shared/marfan/globgm_ss/output/average/gswp3-w5e5/ss/mf6_post/"
+
+# clone map
+clone_map = "/projects/0/dfguu/users/edwin/data/pcrglobwb_input_arise/develop/global_30sec/routing/surface_water_bodies/version_2020-05-XX/lddsound_30sec_version_202005XX.map"
+
+
+# obtain the groundwater head for the bottom layer and their correponding storage
+groundwaterHead1 = None
+storGroundwater1 = None
+for region in range(1, 4):
+    
+    ncFile = modflow6_output_folder + "/s0" + str(region) + "_hds_ss_l1.nc"
+    groundwaterHead1_inp = vos.netcdf2PCRobjCloneWithoutTime(ncFile  = ncFile, \
+                                                             varName = "automatic",\
+                                                             cloneMapFileName  = clone_map,\
+                                                             LatitudeLongitude = True,\
+                                                             specificFillValue = None,\
+                                                             absolutePath = None)
+    if region == 1:
+        groundwaterHead1 = groundwaterHead1_inp
+    else:
+        groundwaterHead1 = pcr.cover(groundwaterHead1, groundwaterHead1_inp)
+    pcr.aguila(groundwaterHead1)
+
 
 number_of_layers = 2
 
+
+# ~ edwinaha@int5:/scratch-shared/marfan/globgm_ss/output/average/gswp3-w5e5/ss/mf6_post$ ls -lah *.nc
+# ~ -rw-r--r--. 1 marfan marfan 1.2G Jun  4 20:50 s01_hds_ss_l1.nc
+# ~ -rw-r--r--. 1 marfan marfan 1.2G Jun  4 20:50 s01_hds_ss_l2.nc
+# ~ -rw-r--r--. 1 marfan marfan 1.2G Jun  4 20:50 s01_wtd_ss_l1.nc
+# ~ -rw-r--r--. 1 marfan marfan 1.2G Jun  4 20:50 s01_wtd_ss_l2.nc
+# ~ -rw-r--r--. 1 marfan marfan 952M Jun  4 20:48 s02_hds_ss_l1.nc
+# ~ -rw-r--r--. 1 marfan marfan 952M Jun  4 20:49 s02_hds_ss_l2.nc
+# ~ -rw-r--r--. 1 marfan marfan 952M Jun  4 20:49 s02_wtd_ss_l1.nc
+# ~ -rw-r--r--. 1 marfan marfan 952M Jun  4 20:49 s02_wtd_ss_l2.nc
+# ~ -rw-r--r--. 1 marfan marfan  64M Jun  4 20:47 s03_hds_ss_l1.nc
+# ~ -rw-r--r--. 1 marfan marfan  64M Jun  4 20:47 s03_hds_ss_l2.nc
+# ~ -rw-r--r--. 1 marfan marfan  64M Jun  4 20:47 s03_wtd_ss_l1.nc
+# ~ -rw-r--r--. 1 marfan marfan  64M Jun  4 20:47 s03_wtd_ss_l2.nc
 
 
 # baseflow (unit: m/day)
---
+++ model_tools_src/python/pcr-globwb/deterministic_runner_for_calculating_baseflow_and_groundwater_storage_develop.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+
+import os
+import shutil
+import sys
+import datetime
+
+import pcraster as pcr
+
+from globgm.virtualOS import vos
+
+# output folder for this analysis
+output_folder = "/scratch-shared/edwinaha/test_baseflow_and_storage/test/"
+
+# make output and temporary folders
+if os.path.exists(output_folder): shutil.rmtree(output_folder)
+os.makedirs(output_folder)
+# - make temporary folder
+tmp_dir = output_folder +  "/tmp/"
+os.makedirs(tmp_dir)
+
+# Note that the output from the MODFLOW6 has the convention that l1 (layer 1) is for the top one, while the pcraster version of GLOBGM use the l1 as the bottom one
+modflow6_output_folder = "/scratch-shared/marfan/globgm_ss/output/average/gswp3-w5e5/ss/mf6_post/"
+
+# clone map
+clone_map = "/projects/0/dfguu/users/edwin/data/pcrglobwb_input_arise/develop/global_30sec/routing/surface_water_bodies/version_2020-05-XX/lddsound_30sec_version_202005XX.map"
+
+
+# obtain the groundwater head for the bottom layer and their correponding storage
+groundwaterHead1 = None
+storGroundwater1 = None
+for region in range(1, 4):
+    
+    ncFile = modflow6_output_folder + "/s0" + str(region) + "_hds_ss_l1.nc"
+    groundwaterHead1_inp = vos.netcdf2PCRobjCloneWithoutTime(ncFile  = ncFile, \
+                                                             varName = "automatic",\
+                                                             cloneMapFileName  = clone_map,\
+                                                             LatitudeLongitude = True,\
+                                                             specificFillValue = None,\
+                                                             absolutePath = None)
+    if region == 1:
+        groundwaterHead1 = groundwaterHead1_inp
+    else:
+        groundwaterHead1 = pcr.cover(groundwaterHead1, groundwaterHead1_inp)
+    pcr.aguila(groundwaterHead1)
+
