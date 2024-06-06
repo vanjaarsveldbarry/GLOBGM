4b1c4d6c9bc58b43cac4ba2706991ef6b5739242
Edwin Sutanudjaja <sutan101@UU078667-ubuntu>
2024-06-06 13:10:16 +0200

    test reading input files from tile folders

--- model_tools_src/python/pcr-globwb/deterministic_runner_for_calculating_baseflow_develop.py
+++ model_tools_src/python/pcr-globwb/deterministic_runner_for_calculating_baseflow_develop.py
@@ -12,7 +12,7 @@ import pcraster as pcr
 import globgm.virtualOS as vos
 
 # output folder for this analysis
-output_folder = "/scratch-shared/edwinaha/test_baseflow_and_storage/test/"
+output_folder = "/scratch-shared/edwinaha/test_baseflow/test/"
 # ~ output_folder = sys.argv[1]
 
 # make output and temporary folders
@@ -28,67 +28,24 @@ modflow6_output_folder   = "/scratch-shared/marfan/globgm_ss/output/average/gswp
 # ~ modflow6_output_folder = sys.argv[2]
 
 # the folder that contain the pcraster maps in tiles
-tile_pcraster_map_folder = "/scratch-shared/edwinbar/globgm_tile_map_files_for_arfan/map_input/steady-state/average/tile_001-163/steady-state_only/maps/"
+tile_pcraster_map_folder = "/scratch-shared/edwinbar/globgm_tile_map_files_for_arfan/map_input/steady-state/average/tile_%03i-163/steady-state_only/maps/"
 
 # clone map
 clone_map = "/projects/0/dfguu/users/edwin/data/pcrglobwb_input_arise/develop/global_30sec/routing/surface_water_bodies/version_2020-05-XX/lddsound_30sec_version_202005XX.map"
 
 
 # parameters for the river package top layer
-river_bed_conductance  =  
-river_stage_elevation  =
-river_bottom_elevation =
-
-
-
-
-# obtain the groundwater head for the top layer (layer 2 of the pcraster version of GLOBGM)
-groundwaterHead2 = None
-for region in range(1, 4):
+river_bed_conductance  = None
+for tile in range(1, 163+1):
+    tile_folder = tile_pcraster_map_folder %(tile)
+    river_bed_conductance_input_file = tile_folder + "/bed_conductance_used.map"
     
-    # Note that the output from the MODFLOW6 has the convention that l1 (layer 1) is for the top one, while the pcraster version of GLOBGM use the l1 as the bottom one
-    ncFile = modflow6_output_folder + "/s0" + str(region) + "_hds_ss_l1.nc"
-
-    print(region)
-    
-    output = tmp_dir + 'temp.map'
-    warp = vos.gdalwarpPCR_with_mv(input = ncFile, output = output, cloneOut = clone_map, tmpDir = tmp_dir, isLddMap = False, isNominalMap = False, miss_val = "-9999")
-    groundwaterHead2_inp = pcr.readmap(output)
-
-
-    if region == 1:
-        groundwaterHead2 = groundwaterHead2_inp
+    if tile == 1: 
+        river_bed_conductance = vos.readPCRmapClone(v = river_bed_conductance_input_file, cloneMapFileName = clone_map, tmpDir = tmp_dir)
     else:
-        groundwaterHead2 = pcr.cover(groundwaterHead2, groundwaterHead2_inp)
-
-    # ~ pcr.aguila(groundwaterHead2)
-
+        river_bed_conductance = pcr.cover(river_bed_conductance, vos.readPCRmapClone(v = river_bed_conductance_input_file, cloneMapFileName = clone_map, tmpDir = tmp_dir))
+    pcr.aguila(river_bed_conductance)    
 
 
-# obtain the groundwater head for the bottom layer (layer 1 of the pcraster version of GLOBGM)
-groundwaterHead1 = None
-for region in range(1, 4):
-    
-    # Note that the output from the MODFLOW6 has the convention that l1 (layer 1) is for the top one, while the pcraster version of GLOBGM use the l1 as the bottom one
-    ncFile = modflow6_output_folder + "/s0" + str(region) + "_hds_ss_l2.nc"
-
-    print(region)
-    
-    output = tmp_dir + 'temp.map'
-    warp = vos.gdalwarpPCR_with_mv(input = ncFile, output = output, cloneOut = clone_map, tmpDir = tmp_dir, isLddMap = False, isNominalMap = False, miss_val = "-9999")
-    groundwaterHead1_inp = pcr.readmap(output)
-
-
-    if region == 1:
-        groundwaterHead1 = groundwaterHead1_inp
-    else:
-        groundwaterHead1 = pcr.cover(groundwaterHead1, groundwaterHead1_inp)
-
-    # ~ pcr.aguila(groundwaterHead1)
-    
-
-
-
-# get the river package outflow
-
-
+river_stage_elevation  = None
+river_bottom_elevation = None
